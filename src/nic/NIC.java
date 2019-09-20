df82770241caafab093235f81f72730cf05631f8
kpsdilshan <Supun@123>
2019-09-20 14:01:55 +0530

    uploading the project to github

---
+++ src/nic/NIC.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nic;
+
+/**
+ *
+ * @author Nishan Chamith
+ */
+public class NIC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       NewFrame n = new NewFrame();
+       n.setVisible(true);
+    }
+}
